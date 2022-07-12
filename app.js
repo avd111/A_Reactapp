a047ca0a3cacfdd5f097b938f2508956213871c1
avd111 <avduevskiy@gmail.com>
2022-07-12 22:22:49 +0300

    2

--- app.js
+++ app.js
@@ -1,22 +1,75 @@
 'use strict';
 
 
+
 const e = React.createElement;
+
+const isTen = (i) => {
+    console.log(i);
+    return i == 10;
+    
+
+}
+
+
+
 
 
 function SuperLikeButton() {
-    const [liked, setLiked] = React.useState(false)
-    console.log(liked);
-    if (liked == true) {
-        return e('span', null, 'You like this')
+    const [count, setCount] = React.useState(0)
+    console.log(count);
+    if (isTen(count)) {
+        return React.createElement('span', null, 'You like this');
     }
+
+    
+    const buttonName = count === 0? 'Like' : `Like ${count}`    
+ 
+
+
     return e(
         'button',
-        { onClick: () => setLiked(true)},
-        'Like'
+        { onClick: () => setCount(count + 1)},
+        buttonName 
       );
     
 }
+
+
+
+
+
+
+function RandomNamesButton(props) {
+    const makeIndex = () => Math.floor(Math.random() * 3);
+    const names = ['Ranger', 'CT', 'Maza'];
+    const [buttonName, setButtonName] = React.useState('Clickme');
+    // const [, forceUpdate] = React.useState();
+    
+
+    
+    // let i = Math.floor(Math.random() * 3);
+
+    // let x = names[i];
+
+    // return e('button', { onClick: () => { console.log('onClick', x); setButtonName(x); forceUpdate();}}, buttonName);
+
+    return e('button', { onClick: () => setButtonName(names[makeIndex()])}, buttonName);
+    
+} 
+    
+        
+
+
+
+    
+
+    
+
+
+
+
+    
 
 
 
@@ -35,5 +88,6 @@ function SuperLikeButton() {
 const domContainer = document.querySelector('#root');
 
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(SuperLikeButton))
+root.render(React.createElement('div', {className: 'Ilya'}, [e(SuperLikeButton), e(RandomNamesButton), e(RandomNamesButton)]));
+
 
